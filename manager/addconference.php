249cd22a2bb31b6ea64fcdbb81bbae7b0978572c
mojiAm <amjadi.mojtaba@gmail.com>
2014-12-20 15:26:25 +0330

    add manager folder

---
+++ manager/addconference.php
@@ -0,0 +1,199 @@
+<?php
+	include_once("../config.php");
+	include_once("../classes/functions.php");
+  	include_once("../classes/messages.php");
+  	include_once("../classes/session.php");	
+  	include_once("../classes/security.php");
+  	include_once("../classes/database.php");	
+	include_once("../classes/login.php");
+	include_once("../lib/persiandate.php"); 
+
+	$login = Login::GetLogin();
+	if (!$login->IsLogged())
+	{
+		header("Location: ../index.php");
+		die(); // solve a security bug
+	}
+	$db = Database::GetDatabase();
+	
+	
+	if ($_POST["mark"]=="savedata")
+	{
+		$fields = array("`title`","`subjects`","`starttime`","`period`","`endtime`","`details`");		
+		$values = array("'{$_POST[edttitle]}'","'{$_POST[edtsubjects]}'","'{$_POST[edtstarttime]}'",
+				"'{$_POST[edtperiod]}'","'{$_POST[edtendtime]}'","'{$_POST[txtdetails]}'");	
+		if (!$db->InsertQuery('defhamayesh',$fields,$values)) 
+		{			
+			header('location:addconference.php?act=new&msg=2');			
+		} 	
+		else 
+		{  
+			header('location:addconference.php?act=new&msg=1');
+		}
+		//echo $db->cmd;
+	}
+	else
+	if ($_POST["mark"]=="editdata")
+	{		
+		
+		$values = array("`title`"=>"'{$_POST[edttitle]}'","`subjects`"=>"'{$_POST[edtsubjects]}'",
+				"`starttime`"=>"'{$_POST[edtstarttime]}'","`period`"=>"'{$_POST[edtperiod]}'",
+				"`endtime`"=>"'{$_POST[edtendtime]}'","`details`"=>"'{$_POST[txtdetails]}'");
+		$db->UpdateQuery("defhamayesh",$values,array("id='{$_GET[did]}'"));
+		
+		header('location:addconference.php?act=new&msg=1');
+		//echo $db->cmd;
+	}
+	
+	if ($_GET['act']=="new")
+	{
+		$insertoredit = "
+			<button id='submit' type='submit' class='btn btn-default'>ثبت</button>
+			<input type='hidden' name='mark' value='savedata' /> ";
+	}
+
+		
+	if ($_GET['act']=="edit")
+	{
+		$row=$db->Select("defhamayesh","*","id='{$_GET["did"]}'",NULL);		
+		$insertoredit = "
+			<button id='submit' type='submit' class='btn btn-default'>ویرایش</button>
+			<input type='hidden' name='mark' value='editdata' /> ";
+	}
+	
+$html=<<<cd
+    <!--Page main section start-->
+    <section id="min-wrapper">
+        <div id="main-content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <!--Top header start-->
+                        <h3 class="ls-top-header">ثبت همایش</h3>
+                        <!--Top header end -->
+                        <!--Top breadcrumb start -->
+                        <ol class="breadcrumb">
+                            <li><a href="javascript:void(0);"><i class="fa fa-home"></i></a></li>
+                            <li class="active">ثبت همایش</li>
+                        </ol>
+                        <!--Top breadcrumb start -->
+                    </div>
+                </div>
+                <!-- Main Content Element  Start-->
+                <form id="frmdata" name="frmdata" enctype="multipart/form-data" action="" method="post" class="form-inline ls_form" role="form">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">عنوان همایش</h3>
+                                </div>
+                                <div class="panel-body">
+                                    <div class="form-group">
+                                        <input id="edttitle" name="edttitle" type="text" class="form-control" value="{$row["title"]}"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div> 
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">سرفصل ها</h3>
+                                </div>
+                                <div class="panel-body">
+                                    <div class="row ls_divider last">
+                                        <div class="col-md-10 ls-group-input">
+                                            <textarea id="edtsubjects" name="edtsubjects" class="animatedTextArea form-control " >
+												{$row["subjects"]}
+											</textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">آغاز همایش (روز و ساعت)</h3>
+                                </div>
+                                <div class="panel-body">
+                                    <div class="form-group">
+                                        <input id="edtstarttime" name="edtstarttime" type="text" class="form-control" value="{$row["starttime"]}"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">مدت زمان همایش</h3>
+                                </div>
+                                <div class="panel-body">
+                                    <div class="form-group">
+                                        <input id="edtperiod" name="edtperiod" type="text" class="form-control" value="{$row["period"]}"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">پایان همایش (روز و ساعت)</h3>
+                                </div>
+                                <div class="panel-body">
+                                    <div class="form-group">
+                                        <input id="edtendtime" name="edtendtime" type="text" class="form-control" value="{$row["endtime"]}"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">توصیف همایش</h3>
+                                </div>
+                                <div class="panel-body">
+                                    <div class="row ls_divider last">
+                                        <div class="col-md-10 ls-group-input">
+                                            <textarea id="txtdetails" name="txtdetails" class="animatedTextArea form-control " >
+												{$row["details"]}
+											</textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div> 
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">ثبت همایش</h3>
+                                </div>
+                                <div class="panel-body">
+                                	{$insertoredit}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                <!-- Main Content Element  End-->
+            </div>
+        </div>
+    </section>
+    <!--Page main section end -->
+cd;
+
+    include_once("./inc/header.php");
+    echo $html;
+    include_once("./inc/footer.php");
+?>
---
+++ manager/img.php
@@ -0,0 +1,29 @@
+<?php
+    include_once("../config.php");
+	include_once("../classes/functions.php");
+  	include_once("../classes/security.php");
+  	include_once("../classes/database.php");	
+
+	$db = Database::GetDatabase();
+	$pic = NULL;	
+	if (isset($_GET["slide"])and $_GET["slide"]=="yes")
+	{
+		$pic = $db->Select("slide","*","`id`='{$_GET[did]}'",NULL);
+	}	
+	else
+	{
+		if (isset($_GET["type"]) and $_GET["type"]=="gall")
+		{
+			$pic = $db->Select("gpics","*","`gid`='{$_GET[did]}'",NULL);
+		}
+		else
+		{
+			$pic = $db->Select("pics","*","`sid`='{$_GET[did]}' AND `tid`='{$_GET[tid]}'",NULL);
+		}
+    }	
+	//echo $db->cmd;
+	header("Content-type: {$pic[itype]}");
+	//echo base64_decode($pic['img']);
+	echo base64_encode($pic['img']);
+	//echo $img;	
+?>
