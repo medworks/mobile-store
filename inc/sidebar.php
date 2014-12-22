9b78831e1c6eaa3e946a835d55b404e28f11b0f9
mojiAm <amjadi.mojtaba@gmail.com>
2014-12-22 15:39:59 +0330

    seprate sidebar

---
+++ inc/sidebar.php
@@ -0,0 +1,456 @@
+<div class="header-container">
+	<header id="header">
+		<div class="banner">
+			<div class="container">
+				<div class="row"></div>
+			</div>
+		</div>
+		<div>
+			<div class="container">
+				<div class="row">
+					<div id="header_logo">
+						<a href="./" title="Demo Store">
+							<img class="logo img-responsive" src="./img/logo.png" alt="فروشگاه موبایل" height="100" width="100">
+						</a>
+					</div>
+					<!-- Block search module TOP -->
+					<!-- <div id="search_block_top" class="col-sm-4 clearfix">
+						<form id="searchbox" method="get" action="">
+							<input name="controller" value="search" type="hidden">
+							<input name="orderby" value="position" type="hidden">
+							<input name="orderway" value="desc" type="hidden">
+							<input kl_virtual_keyboard_secure_input="on" class="search_query form-control" id="search_query_top" name="search_query" value="" type="text">
+							<button type="submit" name="submit_search" class="btn btn-default button-search">
+								<span>جستجو</span>
+							</button>
+						</form>
+					</div> -->
+					<!-- /Block search module TOP -->
+					<!-- MODULE Block cart -->
+					<div class="col-sm-4 block_cart clearfix" style="float:left">
+						<div class="shopping_cart">
+							<a href="http://prestashop-demos.org/PRS06/PRS060144/en/order" title="View my shopping cart" rel="nofollow">
+								<b>سبد خرید</b>
+								<span class="ajax_cart_quantity unvisible">0</span>
+								<span class="ajax_cart_product_txt unvisible">محصول</span>
+								<span class="ajax_cart_product_txt_s unvisible">محصولات</span>
+								<span class="ajax_cart_total unvisible"></span>
+								<span class="ajax_cart_no_product">(خالی)</span>
+							</a>
+							<div class="cart_block block exclusive">
+								<div class="block_content">
+									<!-- block list of products -->
+									<div class="cart_block_list">
+										<p class="cart_block_no_products">
+											بدون محصول
+										</p>
+										<div class="cart-prices">
+											<!-- <div class="cart-prices-line first-line">
+												<span class="price cart_block_shipping_cost ajax_cart_shipping_cost">Free shipping!</span>
+												<span>
+													Shipping
+												</span>
+											</div> -->
+											<div class="cart-prices-line last-line">
+												<span class="price cart_block_total ajax_block_cart_total">0 ریال</span>
+												<span>مجموع</span>
+											</div>
+										</div>
+										<p class="cart-buttons">
+											<a id="button_order_cart" class="btn btn-default button button-small" href="http://prestashop-demos.org/PRS06/PRS060144/en/order" title="Check out" rel="nofollow">
+												<span>پرداخت
+													<i class="icon-chevron-left right"></i>
+												</span>
+											</a>
+										</p>
+									</div>
+								</div>
+							</div><!-- .cart_block -->
+						</div>
+					</div>
+					<div id="layer_cart">
+						<div class="clearfix">
+							<div class="layer_cart_product col-xs-12 col-md-6">
+								<span class="cross" title="Close window"></span>
+								<h2>
+									<i class="icon-ok"></i>Product successfully added to your shopping cart
+								</h2>
+								<div class="product-image-container layer_cart_img">
+								</div>
+								<div class="layer_cart_product_info">
+									<span id="layer_cart_product_title" class="product-name"></span>
+									<span id="layer_cart_product_attributes"></span>
+									<div>
+										<strong class="dark">Quantity</strong>
+										<span id="layer_cart_product_quantity"></span>
+									</div>
+									<div>
+										<strong class="dark">Total</strong>
+										<span id="layer_cart_product_price"></span>
+									</div>
+								</div>
+							</div>
+							<div class="layer_cart_cart col-xs-12 col-md-6">
+								<h2>
+									<!-- Plural Case [both cases are needed because page may be updated in Javascript] -->
+									<span class="ajax_cart_product_txt_s  unvisible">
+										There are <span class="ajax_cart_quantity">0</span> items in your cart.
+									</span>
+									<!-- Singular Case [both cases are needed because page may be updated in Javascript] -->
+									<span class="ajax_cart_product_txt ">
+										There is 1 item in your cart.
+									</span>
+								</h2>
+					
+								<div class="layer_cart_row">
+									<strong class="dark">
+										Total products
+																	(tax excl.)
+															</strong>
+									<span class="ajax_block_products_total">
+															</span>
+								</div>
+					
+												<div class="layer_cart_row">
+									<strong class="dark">
+										Total shipping&nbsp;(tax excl.)
+									</strong>
+									<span class="ajax_cart_shipping_cost">
+																	Free shipping!
+															</span>
+								</div>
+												<div class="layer_cart_row">	
+									<strong class="dark">
+										Total
+																	(tax excl.)
+															</strong>
+									<span class="ajax_block_cart_total">
+															</span>
+								</div>
+								<div class="button-container">	
+									<span class="continue btn btn-default button exclusive-medium" title="Continue shopping">
+										<span>
+											<i class="icon-chevron-left left"></i>Continue shopping
+										</span>
+									</span>
+									<a class="btn btn-default button button-medium" href="http://prestashop-demos.org/PRS06/PRS060144/en/order" title="Proceed to checkout" rel="nofollow">
+										<span>
+											Proceed to checkout<i class="icon-chevron-right right"></i>
+										</span>
+									</a>	
+								</div>
+							</div>
+						</div>
+						<div class="crossseling"></div>
+					</div> <!-- #layer_cart -->
+					<div class="layer_cart_overlay"></div>
+					<!-- /MODULE Block cart -->
+					<div class="clearfix"></div>
+					<!-- Menu -->
+					<div id="block_top_menu" class="sf-contener clearfix col-lg-12">
+						<div class="menu_inner">
+							<div class="cate_inner">
+								<p class="title_block" id="left_categorytitle">
+									<span class="cat_title">محصولات</span>
+									<span class="cate_bullet">&nbsp;</span>
+								</p>
+							</div>
+						
+							<ul class="sf-menu clearfix menu-content">
+								<li>
+									<a href="#" title="">الکترونیکی</a>
+									<ul>
+										<li>
+											<a href="#" title="">موبایل</a>
+											<ul>
+												<li>
+													<a href="#" title="">Nokia Lumia</a>
+												</li>
+												<li>
+													<a href="#" title="">XOlO Q700i</a>
+												</li>
+												<li>
+													<a href="#" title="">Samsung Rex</a>
+												</li>
+												<li>
+													<a href="#" title="">Sony Xperia</a>
+												</li>
+												<li>
+													<a href="#" title="">HTC Desire</a>
+												</li>
+											</ul>
+										</li>
+										<li>
+											<a href="#" title="">صوتی و تصویری</a>
+											<ul>
+												<li>
+													<a href="#" title="">MP3 Players</a>
+												</li>
+												<li>
+													<a href="#" title="">iPods</a>
+												</li>
+												<li>
+													<a href="#" title="">Speakers</a>
+												</li>
+												<li>
+													<a href="#" title="">Video Players</a>
+												</li>
+												<li>
+													<a href="#" title="">All Audio &amp; Video</a>
+												</li>
+											</ul>
+										</li>
+										<li>
+											<a href="#" title="">دوربین</a>
+											<ul>
+												<li>
+													<a href="#" title="">Canon Power Shot</a>
+												</li>
+												<li>
+													<a href="#" title="">Sony Cyber Shot</a>
+												</li>
+												<li>
+													<a href="#" title="">Camcorder</a>
+												</li>
+												<li>
+													<a href="#" title="">Mirrorless</a>
+												</li>
+												<li>
+													<a href="#" title="">Digital Photoframes</a>
+												</li>
+											</ul>
+										</li>
+										<li>
+											<a href="#" title="">لپ تاپ</a>
+											<ul>
+												<li>
+													<a href="#" title="">Dell</a>
+												</li>
+												<li>
+													<a href="#" title="">Lenovo</a>
+												</li>
+												<li>
+													<a href="#" title="">Sony</a>
+												</li>
+												<li>
+													<a href="#" title="">Apple</a>
+												</li>
+												<li>
+													<a href="#" title="">HP</a>
+												</li>
+												<li>
+													<a href="#" title="">Acer</a>
+												</li>
+											</ul>
+										</li>
+										<li>
+											<a href="#" title="">دسکتاپ</a>
+											<ul>
+												<li>
+													<a href="#" title="">Peripherals </a>
+												</li>
+												<li>
+													<a href="#" title="">External Hard Disks</a>
+												</li>
+												<li>
+													<a href="#" title="">Pen Drives </a>
+												</li>
+												<li>
+													<a href="#" title="">Computer Speakers </a>
+												</li>
+												<li>
+													<a href="#" title="">Printer &amp; inks</a>
+												</li>
+												<li>
+													<a href="#" title="">PC Componenet</a>
+												</li>
+											</ul>
+										</li>
+										<li class="category-thumbnail"></li>
+									</ul>
+								</li>
+								<li>
+									<a href="#" title="">لوازم جانبی</a>
+								</li>
+								<li>
+									<a href="#" title="">تجهیزات شبکه</a>
+									<ul>
+										<li>
+											<a href="#" title="">موبایل</a>
+											<ul>
+												<li>
+													<a href="#" title="">Nokia Lumia</a>
+												</li>
+												<li>
+													<a href="#" title="">XOlO Q700i</a>
+												</li>
+												<li>
+													<a href="#" title="">Samsung Rex</a>
+												</li>
+												<li>
+													<a href="#" title="">Sony Xperia</a>
+												</li>
+												<li>
+													<a href="#" title="">HTC Desire</a>
+												</li>
+											</ul>
+										</li>
+										<li>
+											<a href="#" title="">صوتی و تصویری</a>
+											<ul>
+												<li>
+													<a href="#" title="">MP3 Players</a>
+												</li>
+												<li>
+													<a href="#" title="">iPods</a>
+												</li>
+												<li>
+													<a href="#" title="">Speakers</a>
+												</li>
+												<li>
+													<a href="#" title="">Video Players</a>
+												</li>
+												<li>
+													<a href="#" title="">All Audio &amp; Video</a>
+												</li>
+											</ul>
+										</li>
+										<li>
+											<a href="#" title="">دوربین</a>
+											<ul>
+												<li>
+													<a href="#" title="">Canon Power Shot</a>
+												</li>
+												<li>
+													<a href="#" title="">Sony Cyber Shot</a>
+												</li>
+												<li>
+													<a href="#" title="">Camcorder</a>
+												</li>
+												<li>
+													<a href="#" title="">Mirrorless</a>
+												</li>
+												<li>
+													<a href="#" title="">Digital Photoframes</a>
+												</li>
+											</ul>
+										</li>
+										<li>
+											<a href="#" title="">لپ تاپ</a>
+											<ul>
+												<li>
+													<a href="#" title="">Dell</a>
+												</li>
+												<li>
+													<a href="#" title="">Lenovo</a>
+												</li>
+												<li>
+													<a href="#" title="">Sony</a>
+												</li>
+												<li>
+													<a href="#" title="">Apple</a>
+												</li>
+												<li>
+													<a href="#" title="">HP</a>
+												</li>
+												<li>
+													<a href="#" title="">Acer</a>
+												</li>
+											</ul>
+										</li>
+										<li>
+											<a href="#" title="">دسکتاپ</a>
+											<ul>
+												<li>
+													<a href="#" title="">Peripherals </a>
+												</li>
+												<li>
+													<a href="#" title="">External Hard Disks</a>
+												</li>
+												<li>
+													<a href="#" title="">Pen Drives </a>
+												</li>
+												<li>
+													<a href="#" title="">Computer Speakers </a>
+												</li>
+												<li>
+													<a href="#" title="">Printer &amp; inks</a>
+												</li>
+												<li>
+													<a href="#" title="">PC Componenet</a>
+												</li>
+											</ul>
+										</li>
+										<li class="category-thumbnail"></li>
+									</ul>
+								</li>
+								<li>
+									<a href="#" title="">تجهیزات داخلی</a>
+								</li>
+								<li>
+									<a href="#" title="">لوازم دسته دوم</a>
+								</li>
+								<li>
+									<a href="#" title="">لوازم گوشی</a>
+								</li>
+								<li>
+									<a href="#" title="">لوازم کامپیوتر</a>
+								</li>
+								<li>
+									<a href="#" title="">تجهیزات داخلی</a>
+								</li>
+								<li>
+									<a href="#" title="">لوازم گوشی</a>
+								</li>
+								<li>
+									<a href="#" title="">لوازم کامپیوتر</a>
+								</li>
+							</ul>
+						</div>
+						<!--/ Menu -->
+					</div>	
+					<!-- Block links module -->
+					<section class="col-xs-12 col-sm-8" id="tm_links_block4_header">
+						<h4 class="title_block">
+							Block Title4
+						</h4>
+						<div class="block_content toggle-footer">
+							<ul class="bullet">
+									 
+								<li>
+								<a href="" title="">درباره ما</a></li>
+										 
+								<li>
+								<a href="" title="">راهنمای خرید</a></li>
+										 
+								<li>
+								<a href="#" title="">پرداخت امن</a></li>
+										 
+								<li>
+								<a href="" title="">تماس با ما</a></li>
+							</ul>
+						</div>
+					</section>
+					<!-- /Block links module -->
+					<!-- MODULE TM - CMS BLOCK  -->
+					<div id="tmcmsinfo_block">
+						<div class="col-xs-12">
+							<ul>
+								<li class="shipping">
+									<a href="#">حمل و نقل به تمام کشور</a>
+								</li>
+								<li class="Money">
+									<a href="#">گارانتی محصولات</a>
+								</li><!-- 
+								<li class="Offer">
+									<a href="#">پیشنهاد های ویژه</a>
+								</li> -->
+							</ul>
+						</div>
+					</div>
+					<!-- /MODULE TM - CMS BLOCK  -->
+				</div>
+			</div>
+		</div>
+	</header>
+</div>	
