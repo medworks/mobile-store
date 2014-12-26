c8c0fd95e644f277ad01ea83129be3d36b5e5ee8
Saeid Hatami <hatami4510@gmail.com>
2014-12-26 21:54:52 +0330

    needed some times for checking

---
+++ new  1.php
@@ -0,0 +1,274 @@
+<!--
+					<li class=" ajax_block_product col-xs-12 col-sm-4 col-md-3   last-item-of-mobile-line">
+						<div class="product-container" itemscope="" itemtype="http://schema.org/Product">
+							<div class="left-block">
+								<div class="product-image-container">
+									<a class="#" title="" itemprop="url">
+										<img class="replace-2x img-responsive" src="./img/product/2-1.jpg" alt="" title="" itemprop="image" height="173" width="173">
+									</a>
+									<a class="quick-view" href="#" rel="#">
+										<span>نمایش</span>
+									</a>
+									<div class="content_price" itemprop="offers" itemscope="" itemtype="http://schema.org/Offer">
+										<span itemprop="price" class="price product-price">125000 ریال</span>
+										<meta itemprop="priceCurrency" content="1">			
+									</div>
+									<span class="new-box">
+										<span class="new-label">جدید</span>
+									</span>
+								</div>
+							</div>
+							<div class="right-block">
+								<h5 itemprop="name">
+									<a class="product-name" href="#" title="" itemprop="url">
+										آی پد نانو
+									</a>
+								</h5>
+								<div itemprop="offers" itemscope="" itemtype="http://schema.org/Offer" class="content_price">
+									<span itemprop="price" class="price product-price">125000 ریال</span>
+									<meta itemprop="priceCurrency" content="1">
+								</div>
+								<div class="button-container">
+									<a class="button ajax_add_to_cart_button btn btn-default" href="#" rel="nofollow" title="Add to cart" data-id-product="2">
+										<span>اضافه به سبد</span>
+									</a>
+								</div>
+								<div class="product-flags"></div>
+							</div>
+						</div><!-- .product-container> -->
+					</li>							
+					<li class=" ajax_block_product col-xs-12 col-sm-4 col-md-3   last-item-of-tablet-line first-item-of-mobile-line">
+						<div class="product-container" itemscope="" itemtype="http://schema.org/Product">
+							<div class="left-block">
+								<div class="product-image-container">
+									<a class="#" title="Habitasse platea dictumst" itemprop="url">
+										<img class="replace-2x img-responsive" src="./img/product/2-1.jpg" alt="" title="" itemprop="image" height="173" width="173">
+									</a>
+									<a class="quick-view" href="#" rel="#">
+										<span>نمایش</span>
+									</a>
+									<div class="content_price" itemprop="offers" itemscope="" itemtype="http://schema.org/Offer">
+										<span itemprop="price" class="price product-price">125000 ریال</span>
+										<meta itemprop="priceCurrency" content="1">			
+									</div>
+									<span class="new-box">
+										<span class="new-label">جدید</span>
+									</span>
+									<span class="sale-box">
+										<span class="sale-label">فروخته شد!</span>
+									</span>
+								</div>
+							</div>
+							<div class="right-block">
+								<h5 itemprop="name">
+									<a class="product-name" href="#" title="" itemprop="url">
+										آی پاد نانو
+									</a>
+								</h5>
+								<div itemprop="offers" itemscope="" itemtype="http://schema.org/Offer" class="content_price">
+									<span itemprop="price" class="price product-price">125000 ریال</span>
+									<meta itemprop="priceCurrency" content="1">
+								</div>
+								<div class="button-container">
+									<a class="button ajax_add_to_cart_button btn btn-default" href="#" rel="nofollow" title="Add to cart" data-id-product="3">
+										<span>اضافه به سبد</span>
+									</a>
+								</div>
+								<div class="product-flags">
+									<span class="online_only">خرید آنلاین</span>
+								</div>
+							</div>
+						</div>
+						<!-- .product-container> -->
+					</li>
+					<li class=" ajax_block_product col-xs-12 col-sm-4 col-md-3   last-item-of-mobile-line">
+						<div class="product-container" itemscope="" itemtype="http://schema.org/Product">
+							<div class="left-block">
+								<div class="product-image-container">
+									<a class="#" title="" itemprop="url">
+										<img class="replace-2x img-responsive" src="./img/product/2-1.jpg" alt="" title="" itemprop="image" height="173" width="173">
+									</a>
+									<a class="quick-view" href="#" rel="#">
+										<span>نمایش</span>
+									</a>
+									<div class="content_price" itemprop="offers" itemscope="" itemtype="http://schema.org/Offer">
+										<span itemprop="price" class="price product-price">125000 ریال</span>
+										<meta itemprop="priceCurrency" content="1">			
+									</div>
+									<span class="new-box">
+										<span class="new-label">جدید</span>
+									</span>
+								</div>
+							</div>
+							<div class="right-block">
+								<h5 itemprop="name">
+									<a class="product-name" href="#" title="" itemprop="url">
+										آی پد نانو
+									</a>
+								</h5>
+								<div itemprop="offers" itemscope="" itemtype="http://schema.org/Offer" class="content_price">
+									<span itemprop="price" class="price product-price">125000 ریال</span>
+									<meta itemprop="priceCurrency" content="1">
+								</div>
+								<div class="button-container">
+									<a class="button ajax_add_to_cart_button btn btn-default" href="#" rel="nofollow" title="Add to cart" data-id-product="2">
+										<span>اضافه به سبد</span>
+									</a>
+								</div>
+								<div class="product-flags"></div>
+							</div>
+						</div><!-- .product-container> -->
+					</li>	
+					<li class=" ajax_block_product col-xs-12 col-sm-4 col-md-3   last-item-of-mobile-line">
+						<div class="product-container" itemscope="" itemtype="http://schema.org/Product">
+							<div class="left-block">
+								<div class="product-image-container">
+									<a class="#" title="" itemprop="url">
+										<img class="replace-2x img-responsive" src="./img/product/2-1.jpg" alt="" title="" itemprop="image" height="173" width="173">
+									</a>
+									<a class="quick-view" href="#" rel="#">
+										<span>نمایش</span>
+									</a>
+									<div class="content_price" itemprop="offers" itemscope="" itemtype="http://schema.org/Offer">
+										<span itemprop="price" class="price product-price">125000 ریال</span>
+										<meta itemprop="priceCurrency" content="1">			
+									</div>
+									<span class="new-box">
+										<span class="new-label">جدید</span>
+									</span>
+								</div>
+							</div>
+							<div class="right-block">
+								<h5 itemprop="name">
+									<a class="product-name" href="#" title="" itemprop="url">
+										آی پد نانو
+									</a>
+								</h5>
+								<div itemprop="offers" itemscope="" itemtype="http://schema.org/Offer" class="content_price">
+									<span itemprop="price" class="price product-price">125000 ریال</span>
+									<meta itemprop="priceCurrency" content="1">
+								</div>
+								<div class="button-container">
+									<a class="button ajax_add_to_cart_button btn btn-default" href="#" rel="nofollow" title="Add to cart" data-id-product="2">
+										<span>اضافه به سبد</span>
+									</a>
+								</div>
+								<div class="product-flags"></div>
+							</div>
+						</div><!-- .product-container> -->
+					</li>	
+					<li class=" ajax_block_product col-xs-12 col-sm-4 col-md-3   last-item-of-mobile-line">
+						<div class="product-container" itemscope="" itemtype="http://schema.org/Product">
+							<div class="left-block">
+								<div class="product-image-container">
+									<a class="#" title="" itemprop="url">
+										<img class="replace-2x img-responsive" src="./img/product/2-1.jpg" alt="" title="" itemprop="image" height="173" width="173">
+									</a>
+									<a class="quick-view" href="#" rel="#">
+										<span>نمایش</span>
+									</a>
+									<div class="content_price" itemprop="offers" itemscope="" itemtype="http://schema.org/Offer">
+										<span itemprop="price" class="price product-price">125000 ریال</span>
+										<meta itemprop="priceCurrency" content="1">			
+									</div>
+									<span class="new-box">
+										<span class="new-label">جدید</span>
+									</span>
+								</div>
+							</div>
+							<div class="right-block">
+								<h5 itemprop="name">
+									<a class="product-name" href="#" title="" itemprop="url">
+										آی پد نانو
+									</a>
+								</h5>
+								<div itemprop="offers" itemscope="" itemtype="http://schema.org/Offer" class="content_price">
+									<span itemprop="price" class="price product-price">125000 ریال</span>
+									<meta itemprop="priceCurrency" content="1">
+								</div>
+								<div class="button-container">
+									<a class="button ajax_add_to_cart_button btn btn-default" href="#" rel="nofollow" title="Add to cart" data-id-product="2">
+										<span>اضافه به سبد</span>
+									</a>
+								</div>
+								<div class="product-flags"></div>
+							</div>
+						</div><!-- .product-container> -->
+					</li>							
+					<li class=" ajax_block_product col-xs-12 col-sm-4 col-md-3   last-item-of-mobile-line">
+						<div class="product-container" itemscope="" itemtype="http://schema.org/Product">
+							<div class="left-block">
+								<div class="product-image-container">
+									<a class="#" title="" itemprop="url">
+										<img class="replace-2x img-responsive" src="./img/product/2-1.jpg" alt="" title="" itemprop="image" height="173" width="173">
+									</a>
+									<a class="quick-view" href="#" rel="#">
+										<span>نمایش</span>
+									</a>
+									<div class="content_price" itemprop="offers" itemscope="" itemtype="http://schema.org/Offer">
+										<span itemprop="price" class="price product-price">125000 ریال</span>
+										<meta itemprop="priceCurrency" content="1">			
+									</div>
+									<span class="new-box">
+										<span class="new-label">جدید</span>
+									</span>
+								</div>
+							</div>
+							<div class="right-block">
+								<h5 itemprop="name">
+									<a class="product-name" href="#" title="" itemprop="url">
+										آی پد نانو
+									</a>
+								</h5>
+								<div itemprop="offers" itemscope="" itemtype="http://schema.org/Offer" class="content_price">
+									<span itemprop="price" class="price product-price">125000 ریال</span>
+									<meta itemprop="priceCurrency" content="1">
+								</div>
+								<div class="button-container">
+									<a class="button ajax_add_to_cart_button btn btn-default" href="#" rel="nofollow" title="Add to cart" data-id-product="2">
+										<span>اضافه به سبد</span>
+									</a>
+								</div>
+								<div class="product-flags"></div>
+							</div>
+						</div><!-- .product-container> -->
+					</li>							
+					<li class=" ajax_block_product col-xs-12 col-sm-4 col-md-3   last-item-of-mobile-line">
+						<div class="product-container" itemscope="" itemtype="http://schema.org/Product">
+							<div class="left-block">
+								<div class="product-image-container">
+									<a class="#" title="" itemprop="url">
+										<img class="replace-2x img-responsive" src="./img/product/2-1.jpg" alt="" title="" itemprop="image" height="173" width="173">
+									</a>
+									<a class="quick-view" href="#" rel="#">
+										<span>نمایش</span>
+									</a>
+									<div class="content_price" itemprop="offers" itemscope="" itemtype="http://schema.org/Offer">
+										<span itemprop="price" class="price product-price">125000 ریال</span>
+										<meta itemprop="priceCurrency" content="1">			
+									</div>
+									<span class="new-box">
+										<span class="new-label">جدید</span>
+									</span>
+								</div>
+							</div>
+							<div class="right-block">
+								<h5 itemprop="name">
+									<a class="product-name" href="#" title="" itemprop="url">
+										آی پد نانو
+									</a>
+								</h5>
+								<div itemprop="offers" itemscope="" itemtype="http://schema.org/Offer" class="content_price">
+									<span itemprop="price" class="price product-price">125000 ریال</span>
+									<meta itemprop="priceCurrency" content="1">
+								</div>
+								<div class="button-container">
+									<a class="button ajax_add_to_cart_button btn btn-default" href="#" rel="nofollow" title="Add to cart" data-id-product="2">
+										<span>اضافه به سبد</span>
+									</a>
+								</div>
+								<div class="product-flags"></div>
+							</div>
+						</div><!-- .product-container> -->
+					</li>
+    -->
