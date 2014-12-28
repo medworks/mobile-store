4ce8a69d2b0da6c6de3395eb6ff4e1a34fa0ed7f
mojiAm <amjadi.mojtaba@gmail.com>
2014-12-28 09:37:57 +0330

    add order page

---
+++ order.php
@@ -0,0 +1,204 @@
+<?php
+	include_once('./inc/header.php')
+?>
+
+<body id="product" class="product product-20 product-printed-summer-dress category-11 category-camcorder hide-right-column lang_en">
+	<div id="page">
+<?php
+	include_once('./inc/main-sidebar.php')
+?>		
+		
+<div id="center_column" class="center_column col-xs-12" style="width:80%;">				
+	<!-- Breadcrumb -->
+	<div class="breadcrumb clearfix rtl">
+		<a class="home" href="./" title="Return to Home"><i class="icon-home"></i></a>
+		<span class="navigation-pipe">&gt;</span>
+		<a href="javascript:void(0);" title="">الکترونیکی</a>
+		<span class="navigation-pipe">&gt;</span>
+		<a href="javascript:void(0);" title="">دوربین</a>
+		<span class="navigation-pipe">&gt;</span>
+		<a href="javascript:void(0);" title="">سونی</a>
+		<span class="navigation-pipe">&gt;</span>
+		سونی h300 
+	</div>
+	<!-- /Breadcrumb -->
+	<div class="primary_block row" itemscope="" itemtype="http://schema.org/Product">
+		<div class="container">
+			<div class="top-hr"></div>
+		</div>
+		<!-- left infos-->  
+		<div class="pb-left-column col-xs-12 col-sm-4 col-md-5">
+			<!-- product img-->        
+			<div id="image-block" class="clearfix">
+				<span class="new-box">
+					<span class="new-label">جدید</span>
+				</span>
+				<span id="view_full_size">
+					<img id="bigpic" itemprop="image" src="http://prestashop-demos.org/PRS06/PRS060144/199-large_default/printed-summer-dress.jpg" title="" alt="" width="458" height="458">
+					<span class="span_link no-print">نمایش بزرگتر</span>
+				</span>
+			</div> <!-- end image-block -->
+			<!-- thumbnails -->
+			<div id="views_block" class="clearfix ">
+				<span class="view_scroll_spacer">
+					<a id="view_scroll_left" class="" title="Other views" href="javascript:{}">
+						قبلی
+					</a>
+				</span>
+				<div id="thumbs_list">
+					<ul id="thumbs_list_frame" style="width: 1106px;">						<li id="thumbnail_180">
+							<a href="http://prestashop-demos.org/PRS06/PRS060144/180-thickbox_default/printed-summer-dress.jpg" data-fancybox-group="other-views" class="fancybox" title="">
+								<img class="img-responsive" id="thumb_180" src="http://prestashop-demos.org/PRS06/PRS060144/180-cart_default/printed-summer-dress.jpg" alt="" title="" height="60" width="60" itemprop="image">
+							</a>
+						</li>															
+						<li id="thumbnail_199">
+							<a href="http://prestashop-demos.org/PRS06/PRS060144/199-thickbox_default/printed-summer-dress.jpg" data-fancybox-group="other-views" class="fancybox" title="">
+								<img class="img-responsive" id="thumb_199" src="http://prestashop-demos.org/PRS06/PRS060144/199-cart_default/printed-summer-dress.jpg" alt="" title="" height="60" width="60" itemprop="image">
+							</a>
+						</li>
+						<li id="thumbnail_200">
+							<a href="http://prestashop-demos.org/PRS06/PRS060144/200-thickbox_default/printed-summer-dress.jpg" data-fancybox-group="other-views" class="fancybox" title="">
+								<img class="img-responsive" id="thumb_200" src="http://prestashop-demos.org/PRS06/PRS060144/200-cart_default/printed-summer-dress.jpg" alt="" title="" height="60" width="60" itemprop="image">
+							</a>
+						</li>															
+						<li id="thumbnail_201">
+							<a href="http://prestashop-demos.org/PRS06/PRS060144/201-thickbox_default/printed-summer-dress.jpg" data-fancybox-group="other-views" class="fancybox" title="">
+								<img class="img-responsive" id="thumb_201" src="http://prestashop-demos.org/PRS06/PRS060144/201-cart_default/printed-summer-dress.jpg" alt="" title="" height="60" width="60" itemprop="image">
+							</a>
+						</li>															
+						<li id="thumbnail_202">
+							<a href="http://prestashop-demos.org/PRS06/PRS060144/202-thickbox_default/printed-summer-dress.jpg" data-fancybox-group="other-views" class="fancybox" title="">
+								<img class="img-responsive" id="thumb_202" src="http://prestashop-demos.org/PRS06/PRS060144/202-cart_default/printed-summer-dress.jpg" alt="" title="" height="60" width="60" itemprop="image">
+							</a>
+						</li>
+						<li id="thumbnail_203">
+							<a href="http://prestashop-demos.org/PRS06/PRS060144/203-thickbox_default/printed-summer-dress.jpg" data-fancybox-group="other-views" class="fancybox" title="">
+								<img class="img-responsive" id="thumb_203" src="http://prestashop-demos.org/PRS06/PRS060144/203-cart_default/printed-summer-dress.jpg" alt="" title="" height="60" width="60" itemprop="image">
+							</a>
+						</li>
+						<li id="thumbnail_268">
+							<a href="http://prestashop-demos.org/PRS06/PRS060144/268-thickbox_default/printed-summer-dress.jpg" data-fancybox-group="other-views" class="fancybox" title="">
+								<img class="img-responsive" id="thumb_268" src="http://prestashop-demos.org/PRS06/PRS060144/268-cart_default/printed-summer-dress.jpg" alt="" title="" height="60" width="60" itemprop="image">
+							</a>
+						</li>
+						<li id="thumbnail_269">
+							<a href="http://prestashop-demos.org/PRS06/PRS060144/269-thickbox_default/printed-summer-dress.jpg" data-fancybox-group="other-views" class="fancybox" title="">
+								<img class="img-responsive" id="thumb_269" src="http://prestashop-demos.org/PRS06/PRS060144/269-cart_default/printed-summer-dress.jpg" alt="" title=" " height="60" width="60" itemprop="image">
+							</a>
+						</li>
+						<li id="thumbnail_270">
+							<a href="http://prestashop-demos.org/PRS06/PRS060144/270-thickbox_default/printed-summer-dress.jpg" data-fancybox-group="other-views" class="fancybox" title="">
+								<img class="img-responsive" id="thumb_270" src="http://prestashop-demos.org/PRS06/PRS060144/270-cart_default/printed-summer-dress.jpg" alt="" title="" height="60" width="60" itemprop="image">
+							</a>
+						</li>
+						<li id="thumbnail_271">
+							<a href="http://prestashop-demos.org/PRS06/PRS060144/271-thickbox_default/printed-summer-dress.jpg" data-fancybox-group="other-views" class="fancybox" title="">
+								<img class="img-responsive" id="thumb_271" src="http://prestashop-demos.org/PRS06/PRS060144/271-cart_default/printed-summer-dress.jpg" alt="" title="" height="60" width="60" itemprop="image">
+							</a>
+						</li>
+						<li id="thumbnail_289">
+							<a href="http://prestashop-demos.org/PRS06/PRS060144/289-thickbox_default/printed-summer-dress.jpg" data-fancybox-group="other-views" class="fancybox" title="">
+								<img class="img-responsive" id="thumb_289" src="http://prestashop-demos.org/PRS06/PRS060144/289-cart_default/printed-summer-dress.jpg" alt="" title="" height="60" width="60" itemprop="image">
+							</a>
+						</li>
+						<li id="thumbnail_290">
+							<a href="http://prestashop-demos.org/PRS06/PRS060144/290-thickbox_default/printed-summer-dress.jpg" data-fancybox-group="other-views" class="fancybox" title="">
+								<img class="img-responsive" id="thumb_290" src="http://prestashop-demos.org/PRS06/PRS060144/290-cart_default/printed-summer-dress.jpg" alt="" title="" height="60" width="60" itemprop="image">
+							</a>
+						</li>
+						<li id="thumbnail_291">
+							<a href="http://prestashop-demos.org/PRS06/PRS060144/291-thickbox_default/printed-summer-dress.jpg" data-fancybox-group="other-views" class="fancybox" title="">
+								<img class="img-responsive" id="thumb_291" src="http://prestashop-demos.org/PRS06/PRS060144/291-cart_default/printed-summer-dress.jpg" alt="" title="" height="60" width="60" itemprop="image">
+							</a>
+						</li>
+						<li id="thumbnail_292" class="last">
+							<a href="http://prestashop-demos.org/PRS06/PRS060144/292-thickbox_default/printed-summer-dress.jpg" data-fancybox-group="other-views" class="fancybox" title="">
+								<img class="img-responsive" id="thumb_292" src="http://prestashop-demos.org/PRS06/PRS060144/292-cart_default/printed-summer-dress.jpg" alt="" title="" height="60" width="60" itemprop="image">
+							</a>
+						</li>
+					</ul>
+					</div> <!-- end thumbs_list -->					
+						<a id="view_scroll_right" title="Other views" href="javascript:{}">
+							بعدی
+						</a>		
+			</div> <!-- end views-block -->
+			<!-- end thumbnails -->
+		</div> <!-- end pb-left-column -->
+		<!-- end left infos--> 
+		<!-- center infos -->
+		<div class="pb-center-column col-xs-12 col-sm-7">
+			<h1 itemprop="name">سونی h600</h1>
+			<div id="short_description_block">
+				<div id="short_description_content" class="rte align_justify" itemprop="description">
+					<p>
+						سونی h600 توضیحات.... سونی h600 توضیحات.... سونی h600 توضیحات.... سونی h600 توضیحات.... سونی h600 توضیحات.... سونی h600 توضیحات.... سونی h600 توضیحات.... سونی h600 توضیحات.... سونی h600 توضیحات.... سونی h600 توضیحات.... سونی h600 توضیحات.... سونی h600 توضیحات.... سونی h600 توضیحات.... سونی h600 توضیحات.... سونی h600 توضیحات.... سونی h600 توضیحات.... سونی h600 توضیحات.... سونی h600 توضیحات.... سونی h600 توضیحات.... سونی h600 توضیحات.... سونی h600 توضیحات.... 
+					</p>
+				</div>
+				<!---->
+			</div> <!-- end short_description_block -->
+			<!-- pb-right-column-->
+			<div class="pb-right-column">
+				<!-- add to cart form-->
+				<form id="buy_block" action="" method="post">
+					<div class="box-info-product">
+						<div class="content_prices clearfix">
+							<!-- prices -->
+							<div class="price">
+								<p class="our_price_display" itemprop="offers" itemscope="" itemtype="http://schema.org/Offer">
+									<link itemprop="availability" href="http://schema.org/InStock">												<span id="our_price_display" itemprop="price">قیمت: 152,000 ریال</span>
+										<!---->
+								</p>
+							</div> <!-- end prices -->
+							<p id="reduction_amount" style="display: none;">
+								<span id="reduction_amount_display"></span>
+							</p>				 
+							<div class="clear"></div>
+						</div> <!-- end content_prices -->
+						<div class="product_attributes clearfix">
+							<!-- quantity wanted -->
+							<p id="quantity_wanted_p">
+								<label>تعداد:</label>
+								<input type="text" name="qty" id="quantity_wanted" class="text" value="1" style="border: 1px solid rgb(227, 226, 226);">
+								<a href="#" data-field-qty="qty" class="btn btn-default button-minus product_quantity_down">
+									<span><i class="icon-minus"></i></span>
+								</a>
+								<a href="#" data-field-qty="qty" class="btn btn-default button-plus product_quantity_up">
+									<span><i class="icon-plus"></i></span>
+								</a>
+								<span class="clearfix"></span>
+							</p>
+							<p id="quality_wanted_p">
+								<label style="width:40px">کیفیت:</label>
+								<select style="border: 1px solid rgb(227, 226, 226);width:150px;">
+									<option value="0">انتخاب کیفیت</option>
+									<option value="1">اورجینال</option>
+									<option value="2">متوسط</option>
+									<option value="3">پایین</option>
+								</select>
+								<span class="clearfix"></span>
+							</p>
+							<!-- attributes -->
+							<div id="attributes">
+								<div class="clearfix"></div>
+							</div> <!-- end attributes -->
+						</div> <!-- end product_attributes -->
+						<div class="box-cart-bottom">
+							<div>
+								<p id="add_to_cart" class="buttons_bottom_block no-print">
+									<button type="submit" name="Submit" class="exclusive">
+										<span>اضافه به سبد</span>
+									</button>
+								</p>
+							</div>
+						</div> <!-- end box-cart-bottom -->
+					</div> <!-- end box-info-product -->
+				</form>
+			</div> <!-- end pb-right-column-->
+		</div>
+		<!-- end center infos-->	
+	</div> <!-- end primary_block -->
+</div>
+
+<?php
+	include_once('./inc/footer.php');
+?>
