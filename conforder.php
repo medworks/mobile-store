d6f7a85f4d4cc8f78dd09ded4e51ed78897a7dda
Saeid Hatami <hatami4510@gmail.com>
2015-01-19 23:50:15 +0330

    add ability

--- conforder.php
+++ conforder.php
@@ -48,7 +48,7 @@ $html2=<<<cd
 			<span><em>5-</em> کد پیگیری</span>
 		</li>
 	</ul>
-	<!-- /Breadcrumb -->
+<!-- /Breadcrumb -->
 	<div id="order-detail-content" class="table_block table-responsive">
 		<table id="cart_summary" class="table table-bordered rtl">
 			<thead>
@@ -66,7 +66,7 @@ $html2=<<<cd
 				<tr class="cart_total_price">
 					<td rowspan="3" colspan="2" id="cart_voucher" class="cart_voucher"></td>
 					<td colspan="3" class="text-right">مجموع خرید</td>
-					<td colspan="2" class="price" id="total_product">75.00</td>
+					<td colspan="2" class="price" id="total_price_last">0.0</td>
 				</tr>
 				<tr class="cart_total_delivery">
 					<td colspan="3" class="text-right">هزینه حمل و نقل</td>
@@ -77,53 +77,59 @@ $html2=<<<cd
 						<span>مجموعا</span>
 					</td>
 					<td colspan="2" class="price" id="total_price_container">
-						<span id="total_price">377.00</span>
+						<span id="total_price">0.0</span>
 					</td>
 				</tr>
 			</tfoot>
 			<tbody>
-				<tr id="product_1_1_0_0" class="cart_item last_item first_item address_0 odd">
+cd;
+
+$i=0;
+foreach ($_SESSION["products"] as $cart_itm) 
+{
+$i++;
+$subtotal = ($cart_itm["price"]*$cart_itm["qty"]);
+$html2.=<<<cd
+				<tr id="product_1_1_0_0" class="cart_item last_item first_item address_0 odd">					
 					<td class="cart_product">
 						<a href="#"><img src="./img/product/1.jpg" alt="" width="55" height="55"></a>
 					</td>
 					<td class="cart_description">
 						<p class="product-name">
-							<a href="#">عنوان و توضیحات</a>
+							<a href="#">{$cart_itm["name"]}</a>
 						</p>
 				    </td>
 					<td class="cart_unit" data-title="Unit price">
-						<span class="price" id="product_price_1_1_0">
-							<span class="price">125.00</span>
+						<span class="price" id="product_price_{$i}">
+							<span id="product_price_{$i}" class="price singleprice" data-field-price="{$cart_itm["price"]}" >{$cart_itm["price"]}</span>
 						</span>
 					</td>
 					<td id="quantity_wanted_p">
-						<input type="text" name="qty" id="quantity_wanted" class="text" value="1" style="border: 1px solid rgb(227, 226, 226);width:58px;font-size:15px;font-family:'bmitra'">
-						<a href="#" data-field-qty="qty" class="btn btn-default button-minus product_quantity_down" style="margin-top:5px">
+						<input type="text" data-field-row="{$i}" name="qty{$i}" id="qty{$i}" class="text" value="{$cart_itm['qty']}" style="border: 1px solid rgb(227, 226, 226);width:58px;font-size:15px;font-family:'bmitra'">
+						<a href="#" data-field-qty="qty{$i}" data-field-row="{$i}" class="btn btn-default button-minus product_quantity_down" style="margin-top:5px">
 							<span><i class="icon-minus"></i></span>
 						</a>
-						<a href="#" data-field-qty="qty" class="btn btn-default button-plus product_quantity_up" style="margin-top:5px">
+						<a href="#" data-field-qty="qty{$i}" data-field-row="{$i}" class="btn btn-default button-plus product_quantity_up" style="margin-top:5px">
 							<span><i class="icon-plus"></i></span>
 						</a>
 						<span class="clearfix"></span>
 					</td>
 					<td id="quality_wanted_p">
-						<select style="border: 1px solid rgb(227, 226, 226);width:150px;font-size:15px;font-family:'bmitra'">
-							<option value="0">انتخاب کیفیت</option>
-							<option value="1">اورجینال</option>
-							<option value="2">متوسط</option>
-							<option value="3">پایین</option>
-						</select>
+						{$cart_itm["quality"]}
 						<span class="clearfix"></span>
 					</td>
 					<td class="cart_total" data-title="Total">
-						<span class="price" id="total_product_price_1_1_0">375.00</span>
+						<span class="price totalprice" id="total_price_{$i}">{$subtotal}</span>
 					</td>
 					<td class="cart_delete text-center" data-title="Delete">
 						<div>
 							<a rel="nofollow" title="Delete" class="cart_quantity_delete" id="1_1_0_0" href="#"><i class="icon-trash"></i></a>
 						</div>
 					</td>
-				</tr>				
+				</tr>
+cd;
+}
+$html2.=<<<cd
 			</tbody>
 		</table>
 	</div>
@@ -133,6 +139,32 @@ $html2=<<<cd
 		</a>
 	</p>
 </div>
+<script type="text/javascript">
+var sumPrice =0;
+		$(document).ready(function(){
+		
+		$.each($('.totalprice'), function() {    
+			sumPrice += parseInt($(this).text());
+		});		
+		$('#total_price_last').text(sumPrice.toString());
+	    $('#total_price_container').text(sumPrice.toString());
+		
+		 $(".text").keyup(function(){ 
+			row = $(this).data('field-row');			
+            price = $('#product_price_'+row+' span').text()*$(this).val();
+			$('#total_price_'+row).text(price.toString());	
+			sumPrice =0;
+			$.each($('.totalprice'), function() {    
+
+			sumPrice += parseInt($(this).text());
+			});				 
+			   $('#total_price_last').text(sumPrice.toString());
+			   $('#total_price_container').text(sumPrice.toString());
+		
+        });
+				
+		});			
+	</script>
 cd;
 include_once('./inc/header.php');
 echo $html1;
