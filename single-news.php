75fb4a446eb25430eaf8f0b934d016a2d51ff2c2
Saeid Hatami <hatami4510@gmail.com>
2015-01-25 20:22:40 +0330

    add code to single news page

--- single-news.php
+++ single-news.php
@@ -1,12 +1,23 @@
 <?php
-	session_start();	
+	session_start();
+	include_once("config.php");
+	include_once("classes/functions.php");
+  	include_once("classes/security.php");
+  	include_once("classes/database.php");	
+	include_once("./lib/persiandate.php");
+	include_once("./lib/Zebra_Pagination.php");
+	include_once("classes/seo.php");
+			
+	$db = Database::GetDatabase();
+	$news = $db->Select("news","*","id ={$_GET['id']}");
+	$pic = $db->Select("pics","*","`gid`={$news['id']} AND `kind`='2' ");
 
 $html1=<<<cd
 	<body id="product" class="product product-20 product-printed-summer-dress category-11 category-camcorder hide-right-column lang_en">
 	<div id="page">
 cd;
 
-$itemscount = count($_SESSION["products"]);
+
 $html2=<<<cd
 <div id="center_column" class="center_column col-xs-12" style="width:80%;">				
 	<!-- Breadcrumb -->
@@ -17,10 +28,10 @@ $html2=<<<cd
 	</div>
 	<!-- /Breadcrumb -->
 	<div class="rte">
-		<h2 style="margin-bottom:15px;">عنوان خبر</h2>
-		<img src="./img/slides/1.jpg" alt="" width="770px" height="328px" />
+		<h2 style="margin-bottom:15px;">{$news["subject"]}</h2>
+		<img src="./newspics/{$pic['name']}" alt="{$news["subject"]}" width="770px" height="328px" />
 		<p style="font-size:22px;line-height:1.5;margin-top:15px;">
-			توضیحات خبر... توضیحات خبر... توضیحات خبر... توضیحات خبر... توضیحات خبر... توضیحات خبر... توضیحات خبر... توضیحات خبر... توضیحات خبر... توضیحات خبر... توضیحات خبر... توضیحات خبر... توضیحات خبر... توضیحات خبر... 
+			{$news["text"]}
 		</p>
 	</div>
 </div>
